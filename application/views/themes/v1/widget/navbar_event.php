0c502f772b673e5ca52173085e52132ec09e83de
unuha26 <unuha26@users.noreply.github.com>
2018-07-06 19:34:52 +0700

    perbaikan tampilan pada eyeme dan event mobile

--- application/views/themes/v1/widget/navbar_event.php
+++ application/views/themes/v1/widget/navbar_event.php
@@ -1,4 +1,4 @@
-<div class="trending eyevent-kategori">
+<div class="trending eyevent-kategori" style="height: max-content;overflow:  auto;min-height: 44px;">
     <div id="reqtrend" class="loadtrend" action="home" loading="off" clean="clstrend" style="display: block;">
         <span class="x-c" style="float: unset;margin: 0 auto;display: block;">
             <a style="font-weight: 600;" href="<?= base_url(); ?>eyevent/hasil-pertandingan" title="">Hasil Pertandingan</a>
